d7d51afbd06de6f28e8d92ebc5b9f752f729ed88
bokhoshin <gjrjr4545@gmail.com>
2025-03-25 16:45:21 +0900

    [공통] - vm-create 코드 수정 (main.tf)

--- vm-create/main.tf
+++ vm-create/main.tf
@@ -1,10 +1,5 @@
 provider "azurerm" {
   features {}
-
-  client_id       = var.client_id
-  client_secret   = var.client_secret
-  tenant_id       = var.tenant_id
-  subscription_id = var.subscription_id
 }
 
 resource "azurerm_resource_group" "rg" {
@@ -13,42 +8,96 @@ resource "azurerm_resource_group" "rg" {
 }
 
 resource "azurerm_virtual_network" "vnet" {
-  name                = "my-vnet"
+  name                = "sample-vnet"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   address_space       = ["10.0.0.0/16"]
 }
 
 resource "azurerm_subnet" "subnet" {
-  name                 = "my-subnet"
+  name                 = "sample-subnet"
   resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["10.0.1.0/24"]
 }
 
-resource "azurerm_network_interface" "nic" {
-  name                = "my-nic"
+# 공용 IP 주소 리소스 생성
+resource "azurerm_public_ip" "public_ip" {
+  name                         = "sample-public-ip"
+  location                     = azurerm_resource_group.rg.location
+  resource_group_name          = azurerm_resource_group.rg.name
+  allocation_method            = "Static"
+  domain_name_label            = "samplevm${random_string.unique_id.result}"
+}
+
+resource "azurerm_network_security_group" "nsg" {
+  name                = "sample-nsg"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
+
+  security_rule {
+    name                       = "Allow-SSH"
+    priority                   = 1001
+    direction                  = "Inbound"
+    access                    = "Allow"
+    protocol                  = "Tcp"
+    source_port_range         = "*"
+    destination_port_range    = "22"
+    source_address_prefix     = "*"
+    destination_address_prefix = "*"
+  }
+
+  security_rule {
+    name                       = "Allow-HTTPS"
+    priority                   = 1002
+    direction                  = "Inbound"
+    access                    = "Allow"
+    protocol                  = "Tcp"
+    source_port_range         = "*"
+    destination_port_range    = "443"
+    source_address_prefix     = "*"
+    destination_address_prefix = "*"
+  }
+}
+
+# 네트워크 인터페이스에 공용 IP 연결
+resource "azurerm_network_interface" "nic" {
+  name                      = "sample-nic"
+  location                  = azurerm_resource_group.rg.location
+  resource_group_name       = azurerm_resource_group.rg.name
 
   ip_configuration {
     name                          = "internal"
     subnet_id                     = azurerm_subnet.subnet.id
     private_ip_address_allocation = "Dynamic"
   }
+
+  ip_configuration {
+    name                          = "public"
+    subnet_id                     = azurerm_subnet.subnet.id
+    public_ip_address_id          = azurerm_public_ip.public_ip.id
+    private_ip_address_allocation = "Dynamic"
+    primary                       = true   # 기본 구성으로 설정
+  }
 }
 
+resource "azurerm_network_interface_security_group_association" "nic_nsg_association" {
+  network_interface_id      = azurerm_network_interface.nic.id
+  network_security_group_id = azurerm_network_security_group.nsg.id
+}
+
+# 가상 머신 리소스
 resource "azurerm_linux_virtual_machine" "vm" {
-  name                = "my-vm"
+  name                = "sample-vm"
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
-  size                = "Standard_B1s"
+  size                = "Standard_E2bds_v5"
   admin_username      = var.admin_username
 
   network_interface_ids = [azurerm_network_interface.nic.id]
 
   admin_ssh_key {
-    username = var.admin_username
+    username   = var.admin_username
     public_key = file(var.ssh_public_key_path)
   }
 
@@ -59,14 +108,22 @@ resource "azurerm_linux_virtual_machine" "vm" {
 
   source_image_reference {
     publisher = "Canonical"
-    offer     = "UbuntuServer"
-    sku       = "24.04-LTS"
+    offer     = "ubuntu-24_04-lts"
+    sku       = "server"
     version   = "latest"
   }
+  custom_data = base64encode(file("cloud-init.txt"))
+}
+
+# 랜덤 문자열 생성 (도메인 이름에 사용)
+resource "random_string" "unique_id" {
+  length  = 10
+  special = false
+  upper   = false
 }
 
 resource "azurerm_storage_account" "storage" {
-  name                     = "mystorageacct12345"
+  name                     = "mystorage${random_string.unique_id.result}"
   resource_group_name      = azurerm_resource_group.rg.name
   location                 = azurerm_resource_group.rg.location
   account_tier             = "Standard"
