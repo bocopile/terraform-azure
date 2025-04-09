9fb6521992aafd1eb989b6ed210b6771b841bde3
bkshin <gjrjr4545@gmail.com>
2025-04-09 22:23:03 +0900

    compute-gallary 제거

--- vm/vm-create-compute-gallary/output.tf
+++
@@ -1,9 +0,0 @@
-output "vm_public_ip" {
-  description = "Public IP of the virtual machine"
-  value       = azurerm_linux_virtual_machine.vm.public_ip_address
-}
-
-output "storage_account_name" {
-  description = "Storage Account Name"
-  value       = azurerm_storage_account.storage.name
-}
